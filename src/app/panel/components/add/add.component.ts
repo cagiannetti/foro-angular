91e3bbfafca6f37fff64332118e3b4333b1f71b5
Claudio Giannetti <cagiannetti@gmail.com>
2020-09-21 09:59:24 -0300

    clase 213 terminada

--- src/app/panel/components/add/add.component.ts
+++ src/app/panel/components/add/add.component.ts
@@ -3,12 +3,13 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { Topic } from '../../../models/topic';
 import { UserService } from '../../../services/user.service';
+import { TopicService } from '../../../services/topic.service';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css'],
-  providers: [UserService]
+  providers: [UserService, TopicService]
 })
 export class AddComponent implements OnInit {
   public page_title: string;
@@ -20,14 +21,38 @@ export class AddComponent implements OnInit {
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _userService: UserService
+    private _userService: UserService,
+    private _topicService: TopicService
   ) { 
     this.page_title = 'Crear Nuevo Tema';
-    this.identity = this._userService.getIdentity()
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
     this.topic = new Topic('','','','','','',this.identity._id,null); //creamos un objeto vacío
   }
 
   ngOnInit(): void {
+    console.log(this._topicService.prueba());
+  }
+
+  onSubmit(form){
+    //console.log('Datos del form', form);
+    //console.log('Objeto topic relleno: ', this.topic);
+    //console.log('el token: ', this.token);
+    this._topicService.addTopic(this.token, this.topic).subscribe(
+      response => {
+        if(response.topic){
+          this.status='success';
+          this.topic = response.topic;
+          this._router.navigate(['/panel']);
+        }else{
+          this.status='error';
+        }
+      },
+      error => {
+        console.log(error);
+        this.status='error';
+      }
+    );
   }
 
 }
